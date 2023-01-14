015bf07531c3dd2b85115a0d8dd268cdc3c6ffc5
Vadim <3906120@mail.ru>
2023-01-14 23:26:54 +0530

    Добавил условие

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите целое число: ");
+﻿
+/*Напишите программу, которая на вход
+принимает число и выдаёт его квадрат (число
+умноженное на само себя).
+*/
+Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
 Console.WriteLine($"Квадрат числа {number} = {square}");
