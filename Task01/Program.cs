6378eaa389bfb3a9dc70ae993a93257a535cfd83
Vadim <3906120@mail.ru>
2023-01-14 21:59:15 +0530

    added Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 / number2 == number2)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else 
+{
+    Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+}
