abd48e1a8c4952f6ee1879dc4591fa0de61d45e2
Vadim <3906120@mail.ru>
2023-01-15 22:55:14 +0530

    Added else

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -6,14 +6,19 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
-Console.WriteLine("Введите число1: ");
+Console.WriteLine("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число2: ");
+Console.WriteLine("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 if (numberA > numberB)
 {
     Console.WriteLine($"Максимальное число: {numberA}");
     Console.WriteLine($"Минимальное число: {numberB}");
+}
+else 
+{
+    Console.WriteLine($"Максимальное число: {numberB}");
+    Console.WriteLine($"Минимальное число: {numberA}");
 }
