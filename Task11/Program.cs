4a6744bce617d33a2c031df80c6620e0d508abed
Vadim <3906120@mail.ru>
2023-01-18 13:41:57 +0530

    added in Task11 metod

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -6,6 +6,22 @@
 918 -> 98
 */
 
+int RanNumber(int firstDigit, int thirdDigit)
+{
+    return firstDigit * 10 + thirdDigit;
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100-999 => {number}");
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+Console.WriteLine(RanNumber(firstDigit, thirdDigit));
+
+/*
+Вариант без метода
+
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число из отрезка 100-999 => {number}");
 
@@ -15,3 +31,4 @@ int thirdDigit = number % 10;
 int result = firstDigit * 10 + thirdDigit;
 
 Console.WriteLine(result);
+*/
