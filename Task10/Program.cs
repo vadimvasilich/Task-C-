18c098f6b05f5265f72dd048a1891bf17447564d
Vadim <3906120@mail.ru>
2023-01-18 14:40:28 +0530

    added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа. 
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int SecondDigit(int number)
+{
+    return number % 100 / 10;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number < 1000 && number > 99 ) Console.WriteLine(SecondDigit(number));
+else  Console.Write("Это число не трёхзначное!!!");
+
