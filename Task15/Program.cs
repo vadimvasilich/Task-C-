d9ce8dd8bfcab7967c23e94c22a2a07c0b358bc4
Vadim <3906120@mail.ru>
2023-01-21 00:07:40 +0530

    added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool HoliDay(int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// ??? string number = Console.ReadLine();
+
+bool result = HoliDay(number);
+
+Console.Write(result ? "да" : "нет");
+
+//if (number > 7) Console.Write("Вы ввели неправильное число!!!");
