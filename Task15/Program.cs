08da9f02753684c412cbeff83d2e6a2b5a20b14c
Vadim <3906120@mail.ru>
2023-01-22 22:33:26 +0530

    corrected Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -10,12 +10,7 @@ bool HoliDay(int num)
     return num == 6 || num == 7;
 }
 
-Console.WriteLine("Введите число от 1 до 7: ");
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
-// ??? string number = Console.ReadLine();
-
-bool result = HoliDay(number);
-
-Console.Write(result ? "да" : "нет");
-
-//if (number > 7) Console.Write("Вы ввели неправильное число!!!");
+if (number > 1 && number < 7) Console.Write(HoliDay(number) ? "да" : "нет");
+else Console.Write("Вы ввели неправильное число!!!");
