456067973af7160eef02884f2ec82fc38eb70a61
Vadim <3906120@mail.ru>
2023-01-22 18:33:46 +0530

    added Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая заданному номеру четверти  показывает диапазон 
+возможных координат точек в этой четверти (х, у)
+*/
+
+Console.WriteLine("Введите номер четверти координат: ");
+string number = Console.ReadLine();
+
+string quater = Quater(number);
+
+Console.WriteLine($"Интервал координат: {quater}");
+
+string Quater(string num)
+{
+    switch (num)
+    {
+    case "1": return "x > 0 и y > 0";
+    case "2": return "x < 0 и y > 0";
+    case "3": return "x < 0 и y < 0";
+    case "4": return "x > 0 и y < 0";
+    default: return "Ввод не корректен!";
+    }
+//     if (num == "1") return "x > 0 и y > 0";
+//     if (num == "2") return "x < 0 и y > 0";
+//     if (num == "3") return "x < 0 и y < 0";
+//     if (num == "4") return "x > 0 и y < 0";
+//     return "Ввод не корректен!";
+}
