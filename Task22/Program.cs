03f375b1f1ed586c3f4911383333f1924f15c5eb
Vadim <3906120@mail.ru>
2023-01-22 19:53:52 +0530

    added Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимаетна вход число(N)
+ и выдаёт таблицу квадратов чисел от 1 до  N
+*/
+
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) Console.WriteLine("Не корректный ввод!");
+else Table(number);
+
+void Table(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,4}      {count * count,4}");
+        count++;
+    }
+}
