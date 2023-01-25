96d173fdf72ea4651dce76e723582afceace898e
Vadim <3906120@mail.ru>
2023-01-25 12:28:11 +0530

    added Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -14,3 +14,28 @@ for (int i = 0; i < array.Length; i++)
 {
     Console.Write($" {array[i]}");
 }
+
+/* Вариант с методами для разной длины массива:
+
+int[] GenerateArray(int size,int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} " );
+    }
+}
+
+int [] array = GenerateArray(8, 0, 1);
+PrintArray(array);
+*/
