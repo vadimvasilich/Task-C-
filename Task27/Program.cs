d0c65c5114a69be350d8d49b0b6fdc4e85b9547d
Vadim <3906120@mail.ru>
2023-01-27 16:34:49 +0530

    added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+//9012 -> 12
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    for ( ; num != 0; num /= 10)
+    {
+        sum += num % 10; 
+    }
+    return sum;
+} 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = number * -1;
+Console.Write(SumDigit(number));
