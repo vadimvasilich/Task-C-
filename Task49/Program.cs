14c7a2a07a88f10484f3accd68075d7b57e0f292
Vadim <3906120@mail.ru>
2023-02-03 14:01:43 +0530

    added Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задайте двумерный массив. 
+Найдите элементы, у которых оба индекса чётные, и 
+замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть
+вот так:
+1 4 49 2
+5 9 2 3
+64 4 4 4
+*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SquareEvenElements(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(4, 3, 0, 10);
+PrintMatrix(array2D);
+SquareEvenElements(array2D);
+Console.WriteLine();
+PrintMatrix(array2D);
