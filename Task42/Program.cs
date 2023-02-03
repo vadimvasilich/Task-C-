fb86dadd0a564a32ea9ca17552dbd0eb00743aed
Vadim <3906120@mail.ru>
2023-02-03 21:43:46 +0530

    Task42 Преобразование десятичного числа в двоичное

---
+++ Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int BinaryNumber(int num)
+{
+    int res = 0;
+    int count = 1;
+    while ( num > 0)
+    {
+        res += num % 2 * count;
+        num /= 2;
+        count *= 10;
+    }
+    return res;
+}
+int resalt = BinaryNumber(number);
+Console.WriteLine(resalt);
