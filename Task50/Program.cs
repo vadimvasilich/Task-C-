70185fbee07dbfeddff1570cc43ce848efc0a36c
Vadim <3906120@mail.ru>
2023-02-05 15:50:55 +0530

    change Task50 На вход принимает позицию элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет

---
+++ Task50/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+1, 7 -> такого элемента в массиве нет
+*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool FindElement(int x, int y)
+{
+    return x <= 3 || y <= 4;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 0, 9);
+PrintMatrix(array2D);
+
+Console.WriteLine("Введите позицию элемента двумерного масива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+int column = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+bool result = FindElement(row, column);
+Console.WriteLine(result ? array2D[row, column] : "такого элемента в массиве нет");
+
+// if (row <= 3 && column <= 4) Console.WriteLine(array2D[row, column]);
+// else Console.WriteLine(" такого элемента в массиве нет");
