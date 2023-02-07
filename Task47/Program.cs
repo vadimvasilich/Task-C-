28a8177d9cd74d3b67b3df37870c55eef0e76ba4
Vadim <3906120@mail.ru>
2023-02-07 17:32:18 +0530

    change Task47 двумерный массив размером m×n, заполненный случайными вещественными числами.

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -2,9 +2,9 @@
 заполненный случайными вещественными числами.
 m = 3, n = 4.
 
-0,5   7    -2   -0,2
-1   -3,3    8   -9,9
-8    7,8  -7,1   9
+0,5   7     -2    -0,2
+1    -3,3    8    -9,9
+8     7,8   -7,1   9
 */
 
 double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
