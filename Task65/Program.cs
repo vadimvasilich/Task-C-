8c18a351f2418b8831c0ad59074aa5e0eba01452
Vadim <3906120@mail.ru>
2023-02-10 15:47:27 +0530

    Task65 Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.

---
+++ Task65/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте значения M и N. Напишите программу, которая
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 5, 6, 7, 8"
+*/
+
+void GapNaturalNumbers(int num1, int num2)
+{
+    Console.Write($"{num1} ");
+    if(num1 == num2) return;
+    if (num1 > num2)
+    {
+        GapNaturalNumbers(num1-1, num2);
+    }
+    else
+    {
+        GapNaturalNumbers(num1+1, num2);
+    }
+}
+
+Console.WriteLine("Введите два числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1>0 && number2>0) GapNaturalNumbers(number1, number2);
+else Console.WriteLine("Ошибка!!! Число не натуральное.");
