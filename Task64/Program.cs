dcaa52f69fc8d69d550c8cbfb6bb7201da56e901
Vadim <3906120@mail.ru>
2023-02-14 08:22:37 +0530

    Task64 Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -9,15 +9,20 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 void NaturalNumbers(int num)
 {
+
     if (num == 0) return;
     Console.Write($"{num} ");
-    if (num>1) Console.Write(", ");
+    if (num > 1) Console.Write(", ");
     NaturalNumbers(num - 1);
 }
 
 
 Console.WriteLine("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
-if (number > 0) NaturalNumbers(number);
+Console.Write("\"");
+if (number > 0)
+{
+    NaturalNumbers(number);
+    Console.Write("\"");
+}
 else Console.WriteLine("Ошибка!!! Число не натуральное: ");
