88544de09a2ad867aafd6409e1c2bf0c17fed7d7
Vadim <3906120@mail.ru>
2023-02-16 09:42:58 +0530

    Task66 Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте значения M и N. 
+Напишите программу, которая найдёт 
+сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+int SumNaturalNumbers(int numberM, int numberN)
+{
+    if (numberM == numberN + 1) return 0;
+    if (numberM > numberN) return SumNaturalNumbers(numberN, numberM);
+    return numberM += SumNaturalNumbers(numberM + 1, numberN);
+}
+
+Console.WriteLine("Введите два числа:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (numberM > 0 && numberN > 0) Console.WriteLine(SumNaturalNumbers(numberM, numberN));
+else Console.WriteLine("Ошибка!!! Число не натуральное.");
